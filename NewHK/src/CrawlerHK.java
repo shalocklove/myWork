1b371088643889444efe2d9a32e81edfef8f76d1
shalocklove <609145339@qq.com>
2017-08-02 16:03:32 +0800

    Add files via upload

--- NewHK/src/CrawlerHK.java
+++ NewHK/src/CrawlerHK.java
@@ -2,14 +2,11 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import com.Bean.MarkBean;
-import com.Bean.URLBean;
-import com.dao.MarkDAO;
 import com.dao.SparitMarkDAO;
 import com.dao.SparitUrlDAO;
 import com.dao.addUrlDAO;
 
-public class CrawlerHK {
+public class CrawlerHK {//23        from 19280000 to 19307900  time out 2 times in 06.1 take 61 data  140000
 	 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
@@ -17,8 +14,7 @@ public class CrawlerHK {
 		String urlFirst = "";
 		String urlSecend = "";
 		boolean bf = true;
-		URLBean urlBean = null;
-		ThreadPoolExecutor executorFirst = new ThreadPoolExecutor(3, 5, 300,
+		ThreadPoolExecutor executorFirst = new ThreadPoolExecutor(10, 15, 300,
 			       TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(10),
 			    new ThreadPoolExecutor.CallerRunsPolicy());
 		ThreadPoolExecutor executorSecound = new ThreadPoolExecutor(3, 5, 300,
@@ -29,13 +25,14 @@ public class CrawlerHK {
 		threadAddUrl.start();
 		Thread.currentThread().sleep(200);
 		while(bf){
-			if(addURL.getList().size() > 0){
+			if(addURL.getList().size() > 0){//63
 				synchronized(addURL){
 					urlFirst = addURL.getList().get(0);
 					addURL.removeList(urlFirst);
 				}
 				//生成二级页面url
 				executorFirst.execute(new SparitUrlDAO(urlFirst));
+//				Thread.currentThread().sleep(10000);
 				if(SparitUrlDAO.urls.size() > 0){
 					synchronized(SparitUrlDAO.urls.get(0)){
 						urlSecend = SparitUrlDAO.urls.get(0);
@@ -44,9 +41,20 @@ public class CrawlerHK {
 					System.out.println("     :"+urlSecend);
 					executorSecound.execute(new SparitMarkDAO(urlSecend));
 				}
-				
-			}else bf = false;
+			}//else bf = false;
 		}
+//		bf = true;
+//		while(bf){
+//			Thread.currentThread().sleep(200);
+//			if(SparitUrlDAO.urls.size() > 0){
+//				synchronized(SparitUrlDAO.urls.get(0)){
+//					urlSecend = SparitUrlDAO.urls.get(0);
+//					SparitUrlDAO.removeURLBean(urlSecend);
+//				}
+//				System.out.println("     :"+urlSecend);
+//				executorSecound.execute(new SparitMarkDAO(urlSecend));
+//			}else bf = false;
+//		}
 		executorFirst.shutdown();
 		executorSecound.shutdown();
 	}
