f14e61b8d228a95f8bbead40bf2517b45c4cf3ef
shalocklove <609145339@qq.com>
2017-08-04 17:29:53 +0800

    Add files via upload

--- NewHK/src/com/Bean/MarkBean.java
+++ NewHK/src/com/Bean/MarkBean.java
@@ -2,7 +2,10 @@ package com.Bean;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,10 +16,14 @@ public class MarkBean {
 	private String type;//商标种类
 	private String nameAddress;//申请人 姓名／名称、地址
 	private String service;//申请人送达地址
-	private String classNO; // 类别编号
-	private Date date;//提交日期
-	private String name;//申请人姓名
+	private Map<String,String> classNO = new HashMap<String, String>(); // 类别编号
+	private Date date;//提交日期 
+	private String name;//申请人姓名  （owner）
 	private String address;//申请人地址
+	private String markName;//商标名
+	private Date actualdate;//实际日期
+	private Date expirydate;//注册届满日
+	private Map<String, String> matters = new HashMap<String, String>();
 	private Pattern r;
 	private Matcher m;
 	public String getMarkNO() {
@@ -75,16 +82,22 @@ public class MarkBean {
 			this.service = m.group().substring(6);
 		}
 	}
-	public String getClassNO() {
+	public Map<String, String> getClassNO() {
 		return classNO;
 	}
-	public void setClassNO(String classNO) {
-		this.classNO = classNO.substring(59);
+	public void setClass(String classNO) {
+		String[] z = classNO.split("<a name=\"class_no");
+		for(int i = 1; i < z.length; i++){
+			this.classNO.put(z[i].split("</u><br>")[0].substring(z[i].split("</u><br>")[0].length()-2), z[i].split("</u><br>")[1]);
+		}
 	}
 	public Date getDate() {
 		return date;
 	}
 	public void setDate(String date) {
+		r = Pattern.compile("[0-9]{2}-[0-9]{2}-[0-9]{4}");
+		m = r.matcher(date);
+		if(m.find()) date = m.group();
 		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 		
 		try {
@@ -92,6 +105,49 @@ public class MarkBean {
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+	public String getMarkName() {
+		return markName;
+	}
+	public void setMarkName(String markName) {
+		this.markName = markName.split("<br>")[0].split("<td>")[1];
+	}
+	public Date getActualdate() {
+		return actualdate;
+	}
+	public void setActualdate(String actualdate) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		try {
+			this.actualdate = sdf.parse(actualdate.substring(actualdate.length() - 10));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public Date getExpirydate() {
+		return expirydate;
+	}
+	public void setExpirydate(String expirydate) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		try {
+			this.expirydate = sdf.parse(expirydate.substring(expirydate.length() - 10));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public Map<String, String> getMatters() {
+		return matters;
+	}
+	public void setMatters(String matters) {
+		String[] st = matters.split("<td colspan=\"2\">&nbsp</td>");
+		for(int i = 1; i < st.length; i++){
+			String p = st[i].split("<td>")[0].substring(st[i].split("<td>")[0]);
+			System.out.println("V : " + p.length() - 26
+					, st[i].split("<td>")[0].length() - 16));
+			String q = st[i].split("<td>")[1].split(">")[1];
+			System.out.println("S : " + q.substring(1, q.length() - 12));
 		}
 	}
 	
